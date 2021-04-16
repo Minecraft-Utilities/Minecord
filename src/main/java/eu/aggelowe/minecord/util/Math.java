478b1e56bfdf28a3f45c254eea629df6e05e4849
Aggelos Margaritis <aggelos@margaritis.eu>
2021-04-16 15:53:29 +0300

    Added Math

---
+++ src/main/java/eu/aggelowe/minecord/util/Math.java
@@ -0,0 +1,14 @@
+package eu.aggelowe.minecord.util;
+
+/**
+ * <p>
+ * This class contains a lot of methods representing important mathematical
+ * operations of the project. If these methods are changed they might change the
+ * plugin is working so they should only be changed if necessary.
+ * </p>
+ * 
+ * @author Aggelowe
+ */
+public final class Math {
+
+}
